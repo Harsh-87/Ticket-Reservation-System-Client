ce8275d222a3c2e54ba93b32e200a54fe360802d
Harsh Kothari <harsh.kothari@hyperverge.co>
2021-01-22 17:35:22 +0530

    Added authActions to actions folder

---
+++ src/actions/authActions.js
@@ -0,0 +1,49 @@
+import { GET_ERRORS, SET_ADMIN_USER } from "./types";
+import axios from "axios";
+
+export const registerUser = (userdata, history) => (dispatch) => {
+  axios
+    .post("admin/register", userdata)
+    .then((res) => history.push("/login"))
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err,
+      })
+    );
+};
+
+export const loginUser = (userdata) => (dispatch) => {
+  axios
+    .post("admin/login", userdata)
+    .then((res) => {
+      dispatch(setAdminUser(res.data.user));
+    })
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err,
+      })
+    );
+};
+
+export const logoutUser = () => (dispatch) => {
+  axios
+    .get("admin/logout")
+    .then((res) => {
+      dispatch(setAdminUser({}));
+    })
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err,
+      })
+    );
+};
+
+export const setAdminUser = (user) => {
+  return {
+    type: SET_ADMIN_USER,
+    payload: user,
+  };
+};
