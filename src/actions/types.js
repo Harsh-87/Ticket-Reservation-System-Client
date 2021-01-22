cca62b07ae66a72941f0cb8ca36562851207b8e4
Harsh Kothari <harsh.kothari@hyperverge.co>
2021-01-22 20:06:02 +0530

    Added errorsReducer and modified types.js in actions

--- src/actions/types.js
+++ src/actions/types.js
@@ -5,3 +5,4 @@ export const SET_ADMIN_USER = "SET_CURRENT_USER";
 export const ADD_BUS = "ADD_BUS";
 export const GET_BUSES = "GET_BUSES";
 export const GET_BUS = "GET_BUS";
+export const SAVE_TICKET = "SAVE_TICKET";
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import busReducer from "./busReducer";
+import ticketReducer from "./ticketReducer";
+import errorReducer from "./errorsReducer";
 
 export default combineReducers({
   authReducer,
+  busReducer,
+  ticketReducer,
+  errorReducer,
 });
